98af72615004690cc03d85a7e9183f07385daa04
Selvin Omar Castellanos Solares <scastellanos@registromercantil.gob.gt>
2025-01-08 17:13:13 -0600

    Ajustes en prefijos de rutas para evitar conflictos

--- src/routes/actividadRoute.js
+++ src/routes/actividadRoute.js
@@ -10,11 +10,11 @@ import {
 
 const router = Router();
 
-router.get("/actividad/:id_usuario/:id_categoria/:id_idioma", getActividades);
-router.get("/actividad/:id_actividad", getActividad);
-router.post("/actividad", saveActividad);
-router.delete("/actividad/:id_actividad", deleteActividad);
-router.put("/actividad/:id_actividad", updateActividad);
+router.get("/:id_usuario/:id_categoria/:id_idioma", getActividades);
+router.get("/:id_actividad", getActividad);
+router.post("/", saveActividad);
+router.delete("/:id_actividad", deleteActividad);
+router.put("/:id_actividad", updateActividad);
 router.get("/semana", getDiasSemana);
 
 export default router;
